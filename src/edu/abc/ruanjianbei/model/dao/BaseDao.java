7b4ecfa402f668978a7e1129ff85acc776528dbf
sunnybingxue <1018757087@qq.com>
2019-08-30 15:26:16 +0800

    first
    信息手册查重分析

--- src/edu/abc/ruanjianbei/model/dao/BaseDao.java
+++
@@ -1,9 +0,0 @@
-package edu.abc.ruanjianbei.model.dao;
-
-public interface BaseDao {
-	
-	public Boolean add(Object o);
-	public Boolean delete(Object o);
-	public Boolean update(Object o);
-	public Object list();
-}
